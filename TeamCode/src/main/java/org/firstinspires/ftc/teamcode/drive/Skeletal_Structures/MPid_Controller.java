4faf82cf7e1b8e36c0b0e1fd540630909d13ace8
mihnwq <86513396+mihnwq@users.noreply.github.com>
2023-01-26 20:30:19 +0200

    Added a lot of stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Skeletal_Structures/MPid_Controller.java
@@ -0,0 +1,64 @@
+package org.firstinspires.ftc.teamcode.drive.Skeletal_Structures;
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+//Made this to use PID with mp, made in a seperate class since you are using mp only if your normal pid doesn't work as good
+public class MPid_Controller {
+
+    MotionProfile MP = new MotionProfile();
+    ElapsedTime timer = new ElapsedTime();
+
+    double Kp = 0.0;
+    double Kd = 0.0;
+    double Ki = 0.0;
+
+    double maxAccel = 0.0;
+    double maxVel = 0.0;
+
+    double IntegralSum = 0.0;
+    double LastError = 0.0;
+    double LastReference = 0.0;
+
+    public MPid_Controller(double Kp, double Ki, double Kd,double maxAccel,double maxVel)
+    {
+        this.Kp = Kp;
+        this.Ki = Ki;
+        this.Kd = Kd;
+
+        this.maxAccel = maxAccel;
+        this.maxVel = maxVel;
+    }
+
+    public double returnPower(double reference, double state , double velocity){
+
+        double error = reference - state;
+
+        velocity = normalize(velocity);
+
+        double InstantError = MP.motion_profile(maxAccel,maxVel,error,error/velocity);
+
+        if(LastReference != reference)
+        {
+            IntegralSum = 0;
+        }
+
+        IntegralSum += InstantError * timer.seconds();
+
+        double derivative = (InstantError - LastError) / timer.seconds();
+
+        LastError = InstantError;
+        LastReference = reference;
+        timer.reset();
+
+        double outpput = (InstantError * Kp) + (derivative * Kd) + (IntegralSum * Ki);
+
+
+        return outpput;
+    }
+
+    double normalize(double vel)
+    {
+        if(vel < 0 || vel == 0)return 1;
+        else return vel;
+    }
+
+}
