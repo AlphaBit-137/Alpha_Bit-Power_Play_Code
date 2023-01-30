7e213e6f8b733e6fc31dc3b9308d50ff8768399c
mihnwq <86513396+mihnwq@users.noreply.github.com>
2023-01-30 21:17:09 +0200

    Update MotionProfile.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Skeletal_Structures/MotionProfile.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Skeletal_Structures/MotionProfile.java
@@ -8,14 +8,16 @@ public class MotionProfile {
 
            double acceleration_dt = max_velocity / max_acceleration;
 
-          //  double halfway_distance = distance / 2;
+            double halfway_distance = distance / 2;
 
             double acceleration_distance = 0.5 * max_acceleration * Math.pow(acceleration_dt, 2);
 
-           // if (acceleration_distance > halfway_distance) ;
-           // acceleration_dt = Math.sqrt(halfway_distance / (0.5 * max_acceleration));
+            if (acceleration_distance > halfway_distance) ;
+            acceleration_dt = Math.sqrt(halfway_distance / (0.5 * max_acceleration));
 
-            acceleration_dt = Math.sqrt(distance/max_acceleration);
+            acceleration_distance = 0.5 * max_acceleration * Math.pow(acceleration_dt, 2);    
+                
+           // acceleration_dt = Math.sqrt(distance/max_acceleration);
 
             max_velocity = max_acceleration * acceleration_dt;
 
