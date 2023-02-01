b0b756abc3e1c2e8c0adc1eda3be5ddf6e1f3953
mihnwq <86513396+mihnwq@users.noreply.github.com>
2023-02-01 12:10:58 +0200

    solved motion profile, forgor sdk cannot SQRT by a negative number (aka cannot no complex numbers obv)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Skeletal_Structures/MotionProfile.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Skeletal_Structures/MotionProfile.java
@@ -5,6 +5,8 @@ public class MotionProfile {
     public double math_normalizer;
     public double distance_normalizer;
 
+    public double reset;
+
     public double motion_profile(double max_acceleration, double max_velocity, double distance,double current_dt) {
 
         double acceleration_dt = max_velocity / max_acceleration;
