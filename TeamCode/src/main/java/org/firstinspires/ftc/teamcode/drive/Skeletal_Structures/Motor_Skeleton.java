6ca7f98d6dc808fb694ac22d44429c567e273ce5
mihnwq <86513396+mihnwq@users.noreply.github.com>
2023-02-05 20:36:01 +0200

    Finished PID auto tuning (finally)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Skeletal_Structures/Motor_Skeleton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Skeletal_Structures/Motor_Skeleton.java
@@ -19,7 +19,6 @@ public class Motor_Skeleton {
 
         ThisMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-
         RUN_WITH_ENCODERS(using_encoders);
 
         IsReversed(IsReversed);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/structure/Slider.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/structure/Slider.java
@@ -5,19 +5,17 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.drive.Skeletal_Structures.Motor_Skeleton;
-import org.firstinspires.ftc.teamcode.drive.Skeletal_Structures.Pid_Controller;
+import org.firstinspires.ftc.teamcode.drive.Skeletal_Structures.OPid_Controller;
 
 public class Slider {
 
     public double Reference;
 
     public static double Kp = 0.0085;
-    public static double Ki = 0.0;
+    public static double Ki = 0.00002;
     public static double Kd = 0.0;
 
-    public double max_output = 0.85;
-
-    Pid_Controller PID = new Pid_Controller(Kp,Ki,Kd);
+    OPid_Controller PID = new OPid_Controller(Kp, Ki, Kd);
 
     public DcMotorEx slider;
     public DcMotorEx slider2;
@@ -25,7 +23,7 @@ public class Slider {
 
     Junctions current_junction = Junctions.Default;
 
-    public enum Junctions{
+    public enum Junctions {
 
         High(3200),
         Medium(7000),
@@ -35,84 +33,92 @@ public class Slider {
 
         public int ThisPosition = 0;
 
-        Junctions(int ThisPosition)
-        {
+        Junctions(int ThisPosition) {
             this.ThisPosition = ThisPosition;
         }
 
     }
 
     Gamepad Slider_Gamepad;
+    //   Pid_Controller PC = new Pid_Controller(0.0085,0.00002,0.0);
+
 
     public Motor_Skeleton sliderMotor = new Motor_Skeleton(slider);
     public Motor_Skeleton sliderMotor2 = new Motor_Skeleton(slider2);
 
 
-    public void init(HardwareMap ahwMap,Gamepad Slider_Gamepad) {
+    public void init(HardwareMap ahwMap, Gamepad Slider_Gamepad) {
         this.Slider_Gamepad = Slider_Gamepad;
 
-        sliderMotor.init(ahwMap,"Slider",false,false);
-        sliderMotor2.init(ahwMap,"Slider2",false,false);
+        sliderMotor.init(ahwMap, "Slider", false,false);
+        sliderMotor2.init(ahwMap, "Slider2", false,false);
     }
 
-    public double GetSliderPosition()
-    {
+    public double GetSliderPosition() {
         return sliderMotor.MotorCurrentPosition();
     }
 
-    public double GetSlider2Position(){return sliderMotor2.MotorCurrentPosition();}
+    public double GetSlider2Position() {
+        return sliderMotor2.MotorCurrentPosition();
+    }
+
+    public boolean SliderBusy() {
+        return sliderMotor.isBusy();
+    }
+
+    public boolean Slider2Busy() {
+        return sliderMotor2.isBusy();
+    }
 
     public void update() {
 
 
-        if(Slider_Gamepad.right_bumper)
-        {
+        if (Slider_Gamepad.right_bumper) {
             Reference = sliderMotor.MotorCurrentPosition();
             SetSliderPower(1);
-        }else if(Slider_Gamepad.left_bumper)
-        {
+        } else if (Slider_Gamepad.left_bumper) {
             Reference = sliderMotor.MotorCurrentPosition();
             SetSliderPower(-1);
-        }else SetPidPower(Reference);
+        } else SetPidPower(Reference);
 
-        if(Slider_Gamepad.dpad_up)
-        {
+        if (Slider_Gamepad.dpad_up) {
             Reference = 1500;
         }
 
-        if(Slider_Gamepad.dpad_down)
-        {
+        if (Slider_Gamepad.dpad_down) {
             Reference = 0;
         }
-
     }
 
-    public void SetSliderPower(double power)
-    {
+    public void SetSliderPower(double power) {
         sliderMotor.SetPower(power);
         sliderMotor2.SetPower(-power);
     }
 
-    public void SetPidPower(double current_Reference)
-    {
-        double power = PID.returnPower(current_Reference,sliderMotor.MotorCurrentPosition());
+    public double getSlider2Power() {
+        return sliderMotor2.GetPower();
+    }
 
-        power = addons(power);
+    public double getSliderPower() {
+        return sliderMotor.GetPower();
+    }
+
+    public int checker() {
+        if (sliderMotor.MotorCurrentPosition() < 0) return 1;
+        else return 0;
+    }
+
+    public void SetPidPower(double current_Reference) {
+        double power = PID.returnPower(current_Reference, sliderMotor.MotorCurrentPosition());
 
         sliderMotor.SetPower(power);
         sliderMotor2.SetPower(-power);
     }
 
-    double addons(double pow)
-    {
-
-        if(pow > max_output)pow = max_output;
-        else if(pow < -max_output)pow = -max_output;
-
-
-        return pow;
+    public boolean goingUpDirection() {
+        if (getSliderPower() > 0) return true;
+        else return false;
     }
-
 }
 
 
