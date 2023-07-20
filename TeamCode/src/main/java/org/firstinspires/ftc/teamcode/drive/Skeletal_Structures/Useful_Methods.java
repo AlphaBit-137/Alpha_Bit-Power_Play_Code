ded3d89b7d593519e7ab29b2626cac577459f499
mihnwq <86513396+mihnwq@users.noreply.github.com>
2023-07-20 21:46:30 +0300

    Update Useful_Methods.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Skeletal_Structures/Useful_Methods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Skeletal_Structures/Useful_Methods.java
@@ -1,10 +1,1 @@
-package org.firstinspires.ftc.teamcode.drive.Skeletal_Structures;
 
-public class Useful_Methods {
-
-    public static double cmInch = 0.393700787;
-
-    public static double CmToInch(double cm) {
-        return cm * cmInch;
-    }
-}
